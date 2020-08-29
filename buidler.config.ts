c31dc4da62377bbca0b87c65ec408fa74d8faed1
Ariel Barmat <abarmat@gmail.com>
2020-08-29 18:46:54 -0300

    Initial commit

---
+++ buidler.config.ts
@@ -0,0 +1,146 @@
+import { Wallet } from 'ethers'
+import { task, usePlugin } from '@nomiclabs/buidler/config'
+
+// Plugins
+
+usePlugin('@nomiclabs/buidler-ethers')
+usePlugin('@nomiclabs/buidler-etherscan')
+usePlugin('@nomiclabs/buidler-waffle')
+usePlugin('buidler-gas-reporter')
+usePlugin('solidity-coverage')
+
+// Networks
+
+interface NetworkConfig {
+  network: string
+  chainId: number
+  gas?: number | 'auto'
+  gasPrice?: number | 'auto'
+}
+
+const networkConfigs: NetworkConfig[] = [
+  { network: 'mainnet', chainId: 1 },
+  { network: 'ropsten', chainId: 3 },
+  { network: 'rinkeby', chainId: 4 },
+  { network: 'kovan', chainId: 42 },
+]
+
+function getAccountMnemonic() {
+  return process.env.MNEMONIC || ''
+}
+
+function getDefaultProviderURL(network: string) {
+  return `https://${network}.infura.io/v3/${process.env.INFURA_KEY}`
+}
+
+function setupDefaultNetworkProviders(buidlerConfig) {
+  for (const netConfig of networkConfigs) {
+    buidlerConfig.networks[netConfig.network] = {
+      chainId: netConfig.chainId,
+      url: getDefaultProviderURL(netConfig.network),
+      gas: netConfig.gasPrice || 'auto',
+      gasPrice: netConfig.gasPrice || 'auto',
+      accounts: {
+        mnemonic: getAccountMnemonic(),
+      },
+    }
+  }
+}
+
+// Tasks
+
+task('accounts', 'Prints the list of accounts', async (taskArgs, bre) => {
+  const accounts = await bre.ethers.getSigners()
+  for (const account of accounts) {
+    console.log(await account.getAddress())
+  }
+})
+
+
+// Config
+
+const config = {
+  paths: {
+    sources: './contracts',
+    tests: './test',
+    artifacts: './build/contracts',
+  },
+  solc: {
+    version: '0.6.12',
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+  },
+  defaultNetwork: 'buidlerevm',
+  networks: {
+    buidlerevm: {
+      chainId: 1337,
+      loggingEnabled: false,
+      gas: 11000000,
+      gasPrice: 'auto',
+      blockGasLimit: 12000000,
+      accounts: [
+        {
+          privateKey: '0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d',
+          balance: '10000000000000000000000',
+        },
+        {
+          privateKey: '0x6cbed15c793ce57650b9877cf6fa156fbef513c4e6134f022a85b1ffdd59b2a1',
+          balance: '10000000000000000000000',
+        },
+        {
+          privateKey: '0x6370fd033278c143179d81c5526140625662b8daa446c22ee2d73db3707e620c',
+          balance: '10000000000000000000000',
+        },
+        {
+          privateKey: '0x646f1ce2fdad0e6deeeb5c7e8e5543bdde65e86029e2fd9fc169899c440a7913',
+          balance: '10000000000000000000000',
+        },
+        {
+          privateKey: '0xadd53f9a7e588d003326d1cbf9e4a43c061aadd9bc938c843a79e7b4fd2ad743',
+          balance: '10000000000000000000000',
+        },
+        {
+          privateKey: '0x395df67f0c2d2d9fe1ad08d1bc8b6627011959b79c53d7dd6a3536a33ab8a4fd',
+          balance: '10000000000000000000000',
+        },
+        {
+          privateKey: '0xe485d098507f54e7733a205420dfddbe58db035fa577fc294ebd14db90767a52',
+          balance: '10000000000000000000000',
+        },
+        {
+          privateKey: '0xa453611d9419d0e56f499079478fd72c37b251a94bfde4d19872c44cf65386e3',
+          balance: '10000000000000000000000',
+        },
+        {
+          privateKey: '0x829e924fdf021ba3dbbc4225edfece9aca04b929d6e75613329ca6f1d31c0bb4',
+          balance: '10000000000000000000000',
+        },
+        {
+          privateKey: '0xb0057716d5917badaf911b193b12b910811c1497b5bada8d7711f758981c3773',
+          balance: '10000000000000000000000',
+        },
+      ],
+    },
+    ganache: {
+      chainId: 1337,
+      url: 'http://localhost:8545',
+    },
+  },
+  etherscan: {
+    url: process.env.ETHERSCAN_API_URL,
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS ? true : false,
+    showTimeSpent: true,
+    currency: 'USD',
+    outputFile: 'reports/gas-report.log',
+  },
+}
+
+setupDefaultNetworkProviders(config)
+
+export default config
+
